6dd948ca844c61918b031f1d78a5aea207a85a29
Benjamin Fockter <benjamin.fkt@gmail.com>
2022-11-21 18:16:46 +0000

    parse input file

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python3
+import sys
 
 
 
 
 
+def load_input(auction_file: str):
+    try:
+        with open(auction_file, 'r') as reader:
+            for line in reader:
+                print(line.strip())
+    except FileNotFoundError as e:
+        print(e)
+        print("Please enter a valid path.")
+    return
 
 
 
+if __name__ == '__main__':
+    path = sys.argv[1]
+    load_input(path)
 
+    print("UnitPytestGame")
