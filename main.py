04a8183d39a4c7259c78d02604d068196d262e35
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-01-11 16:40:37 +0000

    calc winner from multiple bids

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def print_stats(ending_stats: list):
 
 
 def calculate_winning_stats(item_information: dict):
-
     stats = {}
 
     if len(item_information['bids']) == 1:
@@ -22,17 +21,45 @@ def calculate_winning_stats(item_information: dict):
         stats['highest_bid'] = item_information['bids'][stats['winner']][-1][1]
         stats['lowest_bid'] = item_information['bids'][stats['winner']][0][1]
     elif len(item_information['bids']) > 1:
-        # something
-        pass
+        highest_bidder = -1
+        highest_bid = -1.0
+        second_highest_bid = -1.0
+        total_bids = 0
+        lowest_bid = float('inf')
+
+        for user, bids in item_information['bids'].items():
+            final_bid_time = bids[-1][0]
+            final_bid_amount = bids[-1][1]
+            if final_bid_amount > highest_bid[1]:
+                second_highest_bid = highest_bid
+                highest_bid = bids[-1]
+                highest_bidder = user
+            elif final_bid_amount == highest_bid[1] and final_bid_time < highest_bid[0]:
+                highest_bidder = user
+            elif final_bid_amount > second_highest_bid[1] and final_bid_amount != highest_bid[1]:
+                second_highest_bid = bids[-1]
+            if bids[0][1] < lowest_bid:
+                lowest_bid = bids[0][1]
+            total_bids += len(bids)
+
+        stats['winner'] = highest_bidder
+        stats['sold_status'] = "SOLD"
+        stats['price_to_pay'] = second_highest_bid[1]
+        stats['total_bids'] = total_bids
+        stats['highest_bid'] = highest_bid[1]
+        stats['lowest_bid'] = lowest_bid
     else:
-        # something else
-        pass
+        stats['winner'] = ''
+        stats['sold_status'] = "UNSOLD"
+        stats['price_to_pay'] = 0.0
+        stats['total_bids'] = 0
+        stats['highest_bid'] = 0.0
+        stats['lowest_bid'] = 0.0
 
     return stats
 
 
 def calculate_stats(item: str):
-
     item_record = auction_records[item]
     winning_information = calculate_winning_stats(item_record)
 
@@ -73,8 +100,6 @@ def process_sell(listing: list):
         closing_records.setdefault(int(listing[5]), [])
         closing_records[int(listing[5])].append(listing[3])
 
-    # print(auction_records)
-    # print(closing_records)
 
 def process_bid(bidding: list):
     item = bidding[3]
@@ -84,11 +109,11 @@ def process_bid(bidding: list):
     bidding_user = int(bidding[1])
     bid_amount = float(bidding[4])
 
-    if item_start_time < bid_time < item_close_time:
+    if (item_start_time < bid_time < item_close_time) and (bid_amount > 0):
         # check if the user has bid before
         if bidding_user in auction_records[item]['bids']:
             # check that bid is larger than any previous bids
-            if bid_amount > auction_records[item]['bids'][bidding_user][-1]:
+            if bid_amount > auction_records[item]['bids'][bidding_user][-1][1]:
                 auction_records[item]['bids'][bidding_user].append(bid_amount)
         else:
             auction_records[item]['bids'].setdefault(bidding_user, [])
@@ -97,7 +122,6 @@ def process_bid(bidding: list):
 
 def process_input(line: str):
     split_line = line.split('|')
-    # print(split_line)
 
     # do not process heartbeat messages
     if len(split_line) > 1:
@@ -116,10 +140,8 @@ def load_input(auction_file: list):
     try:
         with open(auction_file[0], 'r') as reader:
             for line in reader:
-                # print(line.strip())
                 process_input(line.strip())
-                print(auction_records)
-
+                print(auction_records) # remove later
     except (FileNotFoundError, IndexError) as e:
         print(e)
         print("Please enter a valid path.")
