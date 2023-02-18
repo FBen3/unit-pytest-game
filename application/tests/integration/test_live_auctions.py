ffd65145d0bb40a9381f3f7d21fb7798d12b83b1
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-02-18 23:24:29 +0000

    finish live auction integration test

--- application/tests/integration/test_live_auctions.py
+++ application/tests/integration/test_live_auctions.py
@@ -11,41 +11,19 @@ DEFAULT_INPUT = os.path.join(os.path.dirname(__file__), 'fixtures/default_input.
 
 class TestAuction(unittest.TestCase):
 
-    @patch('builtins.print')
-    def test_default_input(self, mocked_print):
-        process_input.load_input([DEFAULT_INPUT])
 
-        mocked_print.assert_called_with("20|tv_1||UNSOLD|0.00|2|200.00|150.00")
-
+    def tearDown(self) -> None:
+        # reset auction and closing records
+        auction.auction_records = {}
+        auction.closing_records = {}
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_default_input_2(self, mocked_stdout):
+        process_input.load_input([DEFAULT_INPUT])
         expected_output = "20|toaster_1|8|SOLD|12.50|3|20.00|7.50\n" \
                           "20|tv_1||UNSOLD|0.00|2|200.00|150.00\n"
-        process_input.load_input([DEFAULT_INPUT])
 
         self.assertEqual(mocked_stdout.getvalue(), expected_output)
-
-
-
-
-
-
-
-    # def test_collate_item_information(self):
-    #     pass
-    #
-    # def test_calculate_item_stats(self):
-    #     pass
-    #
-    # def test_close_auction(self):
-    #     pass
-    #
-    # def test_process_sell(self):
-    #     pass
-    #
-    # def test_process_bid(self):
-    #     pass
 
 
 if __name__ == '__main__':
