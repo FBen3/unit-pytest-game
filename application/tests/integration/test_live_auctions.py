814d9609f1ab08910b98864e8d0d7ccbfd520da1
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-02-19 00:52:25 +0000

    refactored test_live_auctions.py
    - fixed class name
    - removed duplicate test
    - kept correct way of import data from ./fixtures

--- application/tests/integration/test_live_auctions.py
+++ application/tests/integration/test_live_auctions.py
@@ -9,7 +9,7 @@ import application.process_input as process_input
 
 DEFAULT_INPUT = os.path.join(os.path.dirname(__file__), 'fixtures/default_input.txt')
 
-class TestAuction(unittest.TestCase):
+class TestLiveAuction(unittest.TestCase):
 
 
     def tearDown(self) -> None:
@@ -19,14 +19,6 @@ class TestAuction(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_input(self, mocked_stdout):
-        process_input.load_input([DEFAULT_INPUT])
-        expected_output = "20|toaster_1|8|SOLD|12.50|3|20.00|7.50\n" \
-                          "20|tv_1||UNSOLD|0.00|2|200.00|150.00\n"
-
-        self.assertEqual(mocked_stdout.getvalue(), expected_output)
-
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_input_2(self, mocked_stdout):
         process_input.load_input([DEFAULT_INPUT])
         expected_output = "20|toaster_1|8|SOLD|12.50|3|20.00|7.50\n" \
                           "20|tv_1||UNSOLD|0.00|2|200.00|150.00\n"
