0d77a0d97be715cebdc8bc31b99dd8fec4af8c13
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-04-13 00:34:33 +0100

    update main

--- application/main.py
+++ application/main.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
-# import sys
-#
-# from process_input import load_input
+import sys
 
-from application.auction_cls import Auction
+from auction_cls import Auction
+
 
 if __name__ == "__main__":
-    # path = sys.argv[1:]
-    # load_input(path)
+    path = sys.argv[1:]
 
+    print(path)
 
-
-
-    auction = Auction()
+    auction = Auction(path)
     auction.start()
-    auction.closing_results()
-
-
+    auction.close()
 
     print("\nUnitPytestGame")
