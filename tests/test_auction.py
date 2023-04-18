248ea03adeccc0a8e9559a55d01c7f94b64c74e0
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-04-18 03:29:53 +0100

    finish testing

--- tests/test_auction.py
+++ tests/test_auction.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+from dataclasses import asdict
 
 from application.auction import Auction
 from application.item import Item
@@ -8,10 +9,12 @@ from application.item import Item
 
 class TestDefaultInput:
 
+
     @classmethod
     def setup_class(cls):
         cls.default_live_auction = [os.path.join(os.path.dirname(__file__), "integration", "default_input.txt")]
 
+    @pytest.mark.integration
     def test_default_live_auction(self, capfd):
         input_file = self.default_live_auction
         auction = Auction(input_file)
@@ -41,16 +44,31 @@ class TestAuction:
 
 
 class TestItem:
-    pass
 
 
+    @pytest.mark.unit
+    def test_post_init_values(self):
+        # input parameters violate expected type
+        test_item = Item("13", "78", "test_item", "5", "15")
 
+        assert asdict(test_item) == {
+            'auction_end_time': 15,
+            'auction_start_time': 13,
+            'bid_count': 0,
+            'bids': {},
+            'item': 'test_item',
+            'reserve_price': 5.0,
+            'seller': 78,
+            'status': 'UNSOLD'
+        }
 
+    @pytest.mark.unit
+    def test_winner(self, capfd):
+        test_item = Item(15, 6, "test_item", 27, 31)
+        test_item.bids = {9: [(16, 8.00), (20, 19.00)]}
+        test_item.bid_count = 2
+        test_item.winner()
 
+        out, err = capfd.readouterr()
 
-
-
-
-
-
-
+        assert out == "31|test_item||UNSOLD|0.00|2|19.00|8.00\n"
