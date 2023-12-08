31f6acef33a08c44369def852daf8e7ddb9bfc37
Benjamin Fockter <benjamin.fkt@gmail.com>
2023-12-08 22:52:38 +0000

    refactor
    - move `initialize_tables` from `main.py`
    - fix NUMERIC data type when creating tables
    to allow for bigger numbers

--- application/db_procedures.py
+++ application/db_procedures.py
@@ -28,7 +28,7 @@ def create_bids_table(connection):
             CREATE TABLE bids (
                 id          SERIAL          PRIMARY KEY,
                 item        VARCHAR(30)     NOT NULL,
-                amount      NUMERIC(4, 2)   NOT NULL,
+                amount      NUMERIC(5, 2)   NOT NULL,
                 bid_time    INTEGER         NOT NULL,
                 bidder      INTEGER         NOT NULL,
                 FOREIGN KEY (item) REFERENCES auction(item)
@@ -43,9 +43,43 @@ def create_auction_table(connection):
             CREATE TABLE auction (
                 item            VARCHAR(30)     PRIMARY KEY NOT NULL,
                 seller          INTEGER                     NOT NULL,
-                reserve_price   NUMERIC(4, 2)               NOT NULL,
+                reserve_price   NUMERIC(5, 2)               NOT NULL,
                 opening_time    INTEGER                     NOT NULL,
                 closing_time    INTEGER                     NOT NULL,
                 status          VARCHAR(6)
             );
         """)
+
+def initialize_tables(save_database: bool):
+    print("Initiating database setup:")
+    if not save_database:
+        try:
+            # connect to database (open & close connections automatically)
+            with psycopg2.connect(**db_conn_params) as conn:
+                # manage database resources (cursor object)
+                with conn.cursor() as cur:
+                    cur.execute("""
+                        SELECT table_name
+                        FROM information_schema.tables
+                        WHERE table_schema = 'public'
+                    """)
+                    tables = cur.fetchall()
+
+                    if tables:
+                        print("\t- Found existing tables")
+                        for table in tables:
+                            cur.execute(f"DROP TABLE {table[0]} CASCADE;")
+                        print(f"\t- Deleted tables: {tables}")
+
+                create_auction_table(conn)
+                create_bids_table(conn)
+
+        except psycopg2.DatabaseError as e:
+            print(f"An error occurred while initializing DB tables: {e}")
+            raise e
+
+        except Exception as e:
+            print(f"Some error occurred: {e}")
+            raise e
+    else:
+        print(f"\t- Using existing database")
