ea381809f15b989de783a645715f24a0fff8a57e
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-02-10 09:31:01 +0000

    more test prep

---
+++ tests/unit/test_inputs.py
@@ -0,0 +1,10 @@
+
+# going to test a number if input variations as part of the first batch of unit tests
+# --- this will be different variations of parameters that are passed to the Auction class for initialization
+# --- and the run() method, for example
+
+
+
+
+
+
