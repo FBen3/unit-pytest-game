f928f3aa5fa4920dab463154972a18dae06969e0
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-02-26 18:09:48 +0000

    Refactor config.py to support alternate paths

--- application/config.py
+++ application/config.py
@@ -2,17 +2,23 @@ import configparser
 import os
 
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-root_dir = os.path.dirname(current_dir)
-ini_config_path = os.path.join(root_dir, "config.ini")
+def fetch_database_params(config_path=None):
+    if not config_path:
+        # default to the project root directory if no specific path is provided
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        root_dir = os.path.dirname(current_dir)
+        config_path = os.path.join(root_dir, "config.ini")
 
-config = configparser.ConfigParser()
-config.read(ini_config_path)
+    config = configparser.ConfigParser()
+    if not config.read(config_path):
+        raise FileNotFoundError(f"Could not find or read the config.ini file at location: {config_path}")
 
-db_conn_params = {
-    "dbname": config["database"]["dbname"],
-    "user": config["database"]["user"],
-    "password": config["database"]["password"],
-    "host": config["database"]["host"],
-    "port": config["database"]["port"],
-}
+    db_conn_params = {
+        "dbname": config["database"]["dbname"],
+        "user": config["database"]["user"],
+        "password": config["database"]["password"],
+        "host": config["database"]["host"],
+        "port": config["database"]["port"],
+    }
+
+    return db_conn_params
