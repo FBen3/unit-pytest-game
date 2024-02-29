8e0b94d9ae09f2e43a33df44425b9ff38bf250dc
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-02-29 14:40:38 +0000

    refactor some notes

--- tests/integration/setup_test_db.py
+++ tests/integration/setup_test_db.py
@@ -9,9 +9,9 @@ admin_db_conn_params = fetch_database_params()
 admin_db_conn_params["dbname"] = "postgres"
 
 def initialize_test_db():
-    # DROP DATABASE and CREATE DATABASE cannot run inside a transaction block. (by default all psycopg2 cursor operations are wrapped in a transaction)
-    # you need to run DROP DATABASE and CREATE DATABASE outside of a transaction block in order to execute it.
-    # thus you need to manually manage the connection (without of wrapping it in a `with`; which automatically start in transaction mode).
+    # DROP DATABASE and CREATE DATABASE cannot run inside a transaction block. (by default, psycopg2 operations are part of transaction in the context of a connection)
+    # you need to run DROP DATABASE and CREATE DATABASE outside of a transaction block in order to execute them.
+    # thus you need to manually manage the connection (without of wrapping it in a `with`; which automatically wraps stuff in transaction).
     # to manually manage the connection: first open it, set autocommit=True before any commands are executed, then manually close it afterward.
     # by setting autocommit to True before executing any cursor operations, you ensure that commands are executed immediately (as opposed to part of a transaction).
     conn = psycopg2.connect(**admin_db_conn_params)
