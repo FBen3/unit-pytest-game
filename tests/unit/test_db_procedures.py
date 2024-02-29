38efb1cbed8ebb3cf82b06fac8dea772ae6bcc72
Benjamin Fockter <benjamin.fkt@gmail.com>
2024-02-29 12:35:22 +0000

    update imports

--- tests/unit/test_db_procedures.py
+++ tests/unit/test_db_procedures.py
@@ -2,7 +2,7 @@ import pytest
 
 from unittest.mock import patch, MagicMock, call
 
-from application.db_procedures import *
+from application.db_procedures import initialize_tables, create_auction_table, process_listing
 
 
 def test_initialize_tables_empty_db():
